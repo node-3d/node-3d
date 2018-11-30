163a9bdc6fb6812c7b93285a588c508c46953019
Luis Blanco <luisblanco1337@gmail.com>
2018-11-30 23:07:09 +0300

    Try sysroot #3

--- _macos.sh
+++ _macos.sh
@@ -4,12 +4,7 @@ cd freeimage
 unzip -qq FreeImage3170.zip -d .
 cd FreeImage
 
-# find / -name stdlib.h
-
-export INC = "INCLUDE_PPC='-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk'"
-export INC = "$INC INCLUDE_I386='-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk'"
-export INC = "$INC INCLUDE_X86_64='-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk'"
-echo INC
+find / -name stdlib.h
 
 make -f Makefile.osx CPP_PPC='g++' INCLUDE_PPC='-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk' libfreeimage-3.17.0.dylib
 
