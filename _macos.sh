881324839d28d1f763ec0b6f0183e3bd6bcc9739
Luis Blanco <luisblanco1337@gmail.com>
2018-11-30 19:17:18 +0300

    :green_heart: Try find #2

--- _macos.sh
+++ _macos.sh
@@ -4,10 +4,7 @@ cd freeimage
 unzip -qq FreeImage3170.zip -d .
 cd FreeImage
 
-xcode-select --install
-open /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg
-
-find /Applications/Xcode.app -name stdlib.h
+find / -name stdlib.h
 
 make -f Makefile.osx CPP_X86_64='g++' CPP_PPC='g++' CPP_I386='g++' libfreeimage-3.17.0.dylib
 ls
