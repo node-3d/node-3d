51088c5916714f933a4d824cadef0e4394fe1720
Luis Blanco <luisblanco1337@gmail.com>
2018-11-30 15:13:18 +0300

    :green_heart: Try override #5

--- _macos.sh
+++ _macos.sh
@@ -4,9 +4,11 @@ cd freeimage
 unzip -qq FreeImage3170.zip -d .
 cd FreeImage
 
-brew install gcc6
+brew install gcc6 >/dev/null
 CC=gcc-6
 CXX=g++-6
+gcc -v
+gcc-6 -v
 
-make -f Makefile.osx CPP_X86_64='g++' CPP_PPC='g++' CPP_I386='g++' libfreeimage-3.17.0.dylib
+make -f Makefile.osx CPP_X86_64='g++' CPP_PPC='g++-6' CPP_I386='g++-6' libfreeimage-3.17.0.dylib
 ls
