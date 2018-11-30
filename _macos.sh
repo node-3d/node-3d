e034cfe3205787054e7ad81c8d95415a79253618
Luis Blanco <luisblanco1337@gmail.com>
2018-11-30 14:24:21 +0300

    :green_heart: Try override

--- _macos.sh
+++ _macos.sh
@@ -4,18 +4,7 @@ cd freeimage
 unzip -qq FreeImage3170.zip -d .
 cd FreeImage
 
-export CC=gcc-4.0
-export CXX=g++-4.0
-
-echo EXPORTED
-g++ -v
-echo VERED1
-
 brew install gcc@4.9
 
-echo INSTALLED
-g++ -v
-echo VERED2
-
-make -f Makefile.osx libfreeimage-3.17.0.dylib
+make -f Makefile.osx libfreeimage-3.17.0.dylib CPP_X86_64='g++'
 ls
