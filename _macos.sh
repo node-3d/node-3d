d376687d9b792ec830ec53168bec703dc54406a5
Luis Blanco <luisblanco1337@gmail.com>
2018-11-30 14:07:20 +0300

    :green_heart: Try brew #2

--- _macos.sh
+++ _macos.sh
@@ -3,10 +3,19 @@ echo "Hello MacOS"
 cd freeimage
 unzip -qq FreeImage3170.zip -d .
 cd FreeImage
+
 export CC=gcc-4.0
 export CXX=g++-4.0
+
 echo EXPORTED
-brew install gcc4
+g++ -v
+echo VERED1
+
+brew install gcc >/dev/null
+
 echo INSTALLED
+g++ -v
+echo VERED2
+
 make -f Makefile.osx libfreeimage-3.17.0.dylib
 ls
