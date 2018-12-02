218a7714255be0e92cab6590906649966da16930
Luis Blanco <luisblanco1337@gmail.com>
2018-12-02 11:50:46 +0300

    Try no perf

--- _macos.sh
+++ _macos.sh
@@ -5,9 +5,9 @@ unzip -qq FreeImage3170.zip -d .
 cd FreeImage
 
 make -f Makefile.osx \
-	CPP_X86_64='g++' \
-	CC_X86_64='gcc' \
-	COMPILERFLAGS_X86_64='-arch x86_64 -D__ANSI__' \
+	CPP_X86_64='g++ -w' \
+	CC_X86_64='gcc -w' \
+	COMPILERFLAGS_X86_64='-arch x86_64 -D__ANSI__ -DDISABLE_PERF_MEASUREMENT' \
 	LIBRARIES_X86_64='-Wl,-syslibroot /Applications/Xcode-9.4.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk' \
 	INCLUDE_X86_64='-isysroot /Applications/Xcode-9.4.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk' \
 	libfreeimage-3.17.0.dylib-x86_64
