d41c7232481383d0a2d98647ee810603c5729de1
Luis Blanco <luisblanco1337@gmail.com>
2018-12-02 11:25:49 +0300

    Try ansi

--- _macos.sh
+++ _macos.sh
@@ -6,6 +6,6 @@ cd FreeImage
 
 find / -name stdlib.h
 
-make -f Makefile.osx CPP_X86_64='g++' CC_X86_64='gcc' INCLUDE_X86_64='-isysroot /Applications/Xcode-9.4.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk' libfreeimage-3.17.0.dylib-x86_64
+make -f Makefile.osx CPP_X86_64='g++' CC_X86_64='gcc' COMPILERFLAGS_X86_64='-arch x86_64 -D__ANSI__' INCLUDE_X86_64='-isysroot /Applications/Xcode-9.4.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk' libfreeimage-3.17.0.dylib-x86_64
 
 ls
