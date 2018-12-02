0ed521bdb423393c1b74311eda0c46e3b0ee1c60
Luis Blanco <luisblanco1337@gmail.com>
2018-12-02 20:36:36 +0300

    :hammer: GLFW

--- osx/glfw.sh
+++ osx/glfw.sh
@@ -4,10 +4,11 @@ cd osx/glfw
 unzip -qq glfw-3.2.1.zip -d .
 cd glfw-3.2.1
 
-cmake -DBUILD_SHARED_LIBS=ON .
+cmake -DBUILD_SHARED_LIBS=ON -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF .
 make
 
 ls
+ls CMake
 
 cd ../..
 # mv glfw/glfw-3.2.1/libfreeimage-3.17.0.dylib-x86_64 freeimage.dylib
