2a2c4e33754ff843c276639efb609fcc386201f6
Luis Blanco <luisblanco1337@gmail.com>
2018-12-02 11:38:57 +0300

    Try libs

--- _macos.sh
+++ _macos.sh
@@ -4,8 +4,14 @@ cd freeimage
 unzip -qq FreeImage3170.zip -d .
 cd FreeImage
 
-find / -name stdlib.h
+SDK="/Applications/Xcode-9.4.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
 
-make -f Makefile.osx CPP_X86_64='g++' CC_X86_64='gcc' COMPILERFLAGS_X86_64='-arch x86_64 -D__ANSI__' INCLUDE_X86_64='-isysroot /Applications/Xcode-9.4.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk' libfreeimage-3.17.0.dylib-x86_64
+make -f Makefile.osx \
+	CPP_X86_64='g++ -w' \
+	CC_X86_64='gcc -w' \
+	COMPILERFLAGS_X86_64='-arch x86_64 -D__ANSI__' \
+	LIBRARIES_X86_64='-Wl,-syslibroot ${SDK}' \
+	INCLUDE_X86_64='-isysroot ${SDK}' \
+	libfreeimage-3.17.0.dylib-x86_64
 
 ls
