64c596fa788ff1f3dc6248eb9321e4bad5c76c2d
Luis Blanco <luisblanco1337@gmail.com>
2018-12-02 15:46:29 +0300

    :hammer: Try flat

--- osx/freeimage.sh
+++ osx/freeimage.sh
@@ -8,7 +8,7 @@ make -f Makefile.osx \
 	CPP_X86_64='g++ -w' \
 	CC_X86_64='gcc -w' \
 	COMPILERFLAGS_X86_64='-arch x86_64 -D__ANSI__ -DDISABLE_PERF_MEASUREMENT' \
-	LIBRARIES_X86_64='-Wl,-syslibroot /Applications/Xcode-9.4.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk' \
+	LIBRARIES_X86_64='-flat_namespace -Wl,-syslibroot /Applications/Xcode-9.4.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk' \
 	INCLUDE_X86_64='-isysroot /Applications/Xcode-9.4.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk' \
 	libfreeimage-3.17.0.dylib-x86_64 \
 	>/dev/null
