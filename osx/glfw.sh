8501ba1cef5e29e61aeb65189eb14ad0ed07e28f
Luis Blanco <luisblanco1337@gmail.com>
2018-12-03 10:00:49 +0300

    Show makefiles

--- osx/glfw.sh
+++ osx/glfw.sh
@@ -4,13 +4,33 @@ cd osx/glfw
 unzip -qq glfw-3.2.1.zip -d .
 cd glfw-3.2.1
 
-cmake -DBUILD_SHARED_LIBS=ON -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF .
-make
+cmake \
+	-DBUILD_SHARED_LIBS=ON \
+	-DGLFW_BUILD_EXAMPLES=OFF \
+	-DGLFW_BUILD_TESTS=OFF \
+	-DGLFW_BUILD_DOCS=OFF \
+	-DGLFW_VULKAN_STATIC=OFF \
+	-DGLFW_USE_CHDIR=OFF \
+	-DGLFW_USE_MENUBAR=OFF \
+	-DGLFW_USE_RETINA=ON \
+	.
 
+# make
+
+echo '======= CMakeFiles ========'
 ls CMakeFiles
+echo '======= src/CMakeFiles/glfw.dir ========'
 ls src/CMakeFiles/glfw.dir
 
+echo '======= CMakeFiles/glfw.dir/Makefile2 ========'
+cat CMakeFiles/Makefile2
+echo '======= src/CMakeFiles/glfw.dir/Makefile2 ========'
+cat src/CMakeFiles/glfw.dir/Makefile2
+echo '======= src/CMakeFiles/glfw.dir/build.make ========'
 cat src/CMakeFiles/glfw.dir/build.make
+echo '======= src/CMakeFiles/glfw.dir/depend.make ========'
+cat src/CMakeFiles/glfw.dir/depend.make
+echo '======= ========'
 
 cd ../..
 mv glfw/glfw-3.2.1/src/libglfw.dylib glfw.dylib
