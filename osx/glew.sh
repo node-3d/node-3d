e56efe804ecb43820678c37d484d87da613937b2
Luis Blanco <luisblanco1337@gmail.com>
2018-12-03 14:02:52 +0300

    :hammer: Add GLEW #2

--- osx/glew.sh
+++ osx/glew.sh
@@ -4,12 +4,10 @@ cd osx/glew
 unzip -qq glew-2.1.0.zip -d .
 cd glew-2.1.0
 
-make glew.lib
-
-ls -la lib
+make LDFLAGS.EXTRA='-install_name "@rpath/glew.dylib" -arch x86_64' glew.lib.shared
 
 cd ../..
-# mv glfw/glew-2.1.0/lib/libglew.3.2.dylib glfw.dylib
+mv glfw/glew-2.1.0/lib/libGLEW.2.1.0.dylib glew.dylib
 cd ..
 
 echo 'GLEW Build Finished'
