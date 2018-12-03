59fb181b496ab011c46a06cf3e7af39458c8077a
Luis Blanco <luisblanco1337@gmail.com>
2018-12-03 12:17:36 +0300

    :hammer: Use rpath #8

--- osx/glfw.sh
+++ osx/glfw.sh
@@ -17,19 +17,7 @@ cmake \
 	-DCMAKE_SHARED_LINKER_FLAGS='-install_name "@rpath/glfw.dylib"' \
 	.
 
-echo '/Applications/Xcode-9.4.1.app/Contents/Developer/Toolchains/\
-XcodeDefault.xctoolchain/usr/bin/clang  \
--dynamiclib -Wl,-headerpad_max_install_names \
--install_name "@rpath/glfw.dylib" -compatibility_version 3.0.0 \
--current_version 3.2.0 -o libglfw.3.2.dylib \
-CMakeFiles/glfw.dir/context.c.o CMakeFiles/glfw.dir/init.c.o \
-CMakeFiles/glfw.dir/input.c.o CMakeFiles/glfw.dir/monitor.c.o \
-CMakeFiles/glfw.dir/vulkan.c.o CMakeFiles/glfw.dir/window.c.o \
-CMakeFiles/glfw.dir/cocoa_init.m.o CMakeFiles/glfw.dir/cocoa_joystick.m.o \
-CMakeFiles/glfw.dir/cocoa_monitor.m.o CMakeFiles/glfw.dir/cocoa_window.m.o \
-CMakeFiles/glfw.dir/cocoa_time.c.o CMakeFiles/glfw.dir/posix_tls.c.o \
-CMakeFiles/glfw.dir/nsgl_context.m.o \
--framework Cocoa -framework IOKit -framework CoreFoundation -framework CoreVideo' \
+echo '/Applications/Xcode-9.4.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang  -dynamiclib -Wl,-headerpad_max_install_names -install_name "@rpath/glfw.dylib" -compatibility_version 3.0.0 -current_version 3.2.0 -o libglfw.3.2.dylib CMakeFiles/glfw.dir/context.c.o CMakeFiles/glfw.dir/init.c.o CMakeFiles/glfw.dir/input.c.o CMakeFiles/glfw.dir/monitor.c.o CMakeFiles/glfw.dir/vulkan.c.o CMakeFiles/glfw.dir/window.c.o CMakeFiles/glfw.dir/cocoa_init.m.o CMakeFiles/glfw.dir/cocoa_joystick.m.o CMakeFiles/glfw.dir/cocoa_monitor.m.o CMakeFiles/glfw.dir/cocoa_window.m.o CMakeFiles/glfw.dir/cocoa_time.c.o CMakeFiles/glfw.dir/posix_tls.c.o CMakeFiles/glfw.dir/nsgl_context.m.o -framework Cocoa -framework IOKit -framework CoreFoundation -framework CoreVideo' \
 > src/CMakeFiles/glfw.dir/link.txt
 
 
