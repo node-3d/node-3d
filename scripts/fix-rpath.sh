6837b0dd44f610189d6600f78823c829255c76f2
Luis Blanco <luisblanco1337@gmail.com>
2024-11-15 14:59:38 +0400

    Add libcocoa rpath

--- scripts/fix-rpath.sh
+++ scripts/fix-rpath.sh
@@ -4,9 +4,9 @@ PATH3="@loader_path/../../deps-qt-core-raub/bin-osx"
 PATH4="@loader_path/../node_modules/deps-qt-gui-raub/bin-osx"
 PATH5="@loader_path/../../deps-qt-gui-raub/bin-osx"
 
-install_name_tool -add_rpath "$PATH1" "libqcocoa.dylib" \
-&& install_name_tool -add_rpath "$PATH2" "libqcocoa.dylib" \
-&& install_name_tool -add_rpath "$PATH3" "libqcocoa.dylib" \
-&& install_name_tool -add_rpath "$PATH4" "libqcocoa.dylib" \
-&& install_name_tool -add_rpath "$PATH5" "libqcocoa.dylib" \
+install_name_tool -add_rpath "$PATH1" "$1" \
+&& install_name_tool -add_rpath "$PATH2" "$1" \
+&& install_name_tool -add_rpath "$PATH3" "$1" \
+&& install_name_tool -add_rpath "$PATH4" "$1" \
+&& install_name_tool -add_rpath "$PATH5" "$1" \
 || echo "Rpath already present."
